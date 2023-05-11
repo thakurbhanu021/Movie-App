03b1c918c919ac5b6e6ba64f49783cb75fe08650
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-05-11 21:32:28 +0530

    calling API from react app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import MovieList from "./Components/MovieList";
+import MovieListHeading from "./Components/MovieListHeading";
+import SearchBox from "./Components/SearchBox";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
-  const getMovieRequest = async()=> {
-    const url = 'http://www.omdbapi.com/?s=star wars&apikey=a831a238';
+  const getMovieRequest = async (value) => {
+    const url = `http://www.omdbapi.com/?s=${value}&apikey=a831a238`;
     const response = await fetch(url);
     const responseJson = await response.json();
 
-    console.log(responseJson);
-    setMovies(responseJson.Search);
-  }
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
 
-  useEffect(()=>{
-    getMovieRequest();
-  },[])
-
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
   return (
     <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox value={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
---
+++ src/Components/SearchBox.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SearchBox = (props) => {
+  return (
+    <div className="col col-sm-4">
+      <input
+        className="form-control"
+        type="text"
+        placeholder="Type to search..."
+        value={props.value}
+        onChange={(event)=> props.setSearchValue(event.target.value)}
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
